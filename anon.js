1759c9d3dfedff2e5744bd990efa084fe645c6cb
TheSkyKitty <jonada.ssai@gmail.com>
2023-06-26 19:07:18 +0200

    Delete anon.js

--- anon.js
+++
@@ -1,9 +0,0 @@
-// Externí soubor: script.js
-
-// Definice anonymní funkce
-const myFunction = function() {
-    console.log("Toto je anonymní funkce.");
-  }
-  
-  // Export anonymní funkce pomocí CommonJS (Node.js)
-  module.exports = myFunction;
