808a52273b432fbaac2a74f9a70e720c7c6d4c94
TheSkyKitty <jonada.ssai@gmail.com>
2023-06-26 19:06:30 +0200

    Everything is working rn

---
+++ anon.js
@@ -0,0 +1,9 @@
+// Externí soubor: script.js
+
+// Definice anonymní funkce
+const myFunction = function() {
+    console.log("Toto je anonymní funkce.");
+  }
+  
+  // Export anonymní funkce pomocí CommonJS (Node.js)
+  module.exports = myFunction;
