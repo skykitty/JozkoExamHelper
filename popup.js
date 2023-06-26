d85ddaee04ccd57b426fa29082815894dc9c134c
TheSkyKitty <jonada.ssai@gmail.com>
2023-06-26 18:44:05 +0200

    commitos

--- popup.js
+++ popup.js
@@ -1,11 +1,4 @@
-// Definujte seznam otázek a odpovědí
-const data = [
-  { otazka: 'První otázka', odpoved: 'První odpověď' },
-  { otazka: 'Druhá otázka', odpoved: 'Druhá odpověď' },
-  { otazka: 'Třetí otázka', odpoved: 'Třetí odpověď' },
-  // Přidejte další otázky a odpovědi podle potřeby
-];
-
+import data from './data.js';
 // Funkce pro vykreslení tabulky s otázkami a odpověďmi
 function renderTable() {
   const table = document.getElementById('question-table');
