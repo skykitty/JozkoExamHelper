4fa188cc63bc2532d79de3de96a085c7568bdac8
TheSkyKitty <jonada.ssai@gmail.com>
2023-06-26 18:53:43 +0200

    back on ride

--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
-import data from './data.js';
+const data = [
+  { otazka: 'První otázka', odpoved: 'První odpověď' },
+  { otazka: 'Druhá otázka', odpoved: 'Druhá odpověď' },
+  { otazka: 'Třetí otázka', odpoved: 'Třetí odpověď' },
+  // Přidejte další otázky a odpovědi podle potřeby
+]
 // Funkce pro vykreslení tabulky s otázkami a odpověďmi
 function renderTable() {
   const table = document.getElementById('question-table');
